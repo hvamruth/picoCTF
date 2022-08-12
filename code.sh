a0cef850aeca07bd51e095f892a5deaa3b99feb9
Amruth Hv <79706293+hvamruth@users.noreply.github.com>
2022-08-12 12:55:34 +0530

    Add files via upload

---
+++ code.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+echo "Enter the value of n"
+read n
+a=0
+b=1
+count=2
+
+echo "Fibonacci series:"
+echo $a
+echo $b
+while [ $count -le $n ]
+do
+fib=`expr $a + $b`
+a=$b
+b=$fib
+count=`expr $count + 1`
+
+done
