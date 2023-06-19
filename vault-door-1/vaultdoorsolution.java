396ec3affd7b24088f2bcb384f951c095781fc27
Amruth Hv <79706293+hvamruth@users.noreply.github.com>
2023-06-19 13:16:48 +0530

    Add files via upload

---
+++ vault-door-1/vaultdoorsolution.java
@@ -0,0 +1,43 @@
+public class vaultdoorsolution {
+
+    public static void main(String[] args) {
+        char[] Password = new char [32];
+        Password[0]  = 'd'; 
+        Password[29] = '3'; 
+        Password[4]  = 'r'; 
+        Password[2]  = '5'; 
+        Password[23] = 'r'; 
+        Password[3]  = 'c'; 
+        Password[17] = '4'; 
+        Password[1]  = '3'; 
+        Password[7]  = 'b'; 
+        Password[10] = '_'; 
+        Password[5]  = '4'; 
+        Password[9]  = '3'; 
+        Password[11] = 't'; 
+        Password[15] = 'c'; 
+        Password[8]  = 'l'; 
+        Password[12] = 'H'; 
+        Password[20] = 'c'; 
+        Password[14] = '_'; 
+        Password[6]  = 'm'; 
+        Password[24] = '5'; 
+        Password[18] = 'r'; 
+        Password[13] = '3'; 
+        Password[19] = '4'; 
+        Password[21] = 'T'; 
+        Password[16] = 'H'; 
+        Password[27] = 'f'; 
+        Password[30] = 'b'; 
+        Password[25] = '_'; 
+        Password[22] = '3'; 
+        Password[28] = '6'; 
+        Password[26] = 'f'; 
+        Password[31] = '0';
+        
+        System.out.print(Password);
+
+    }
+
+}
+ 
