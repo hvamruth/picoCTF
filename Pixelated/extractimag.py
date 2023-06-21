f0a3456bb0f786f002165613323d454755b0dc40
Amruth Hv <79706293+hvamruth@users.noreply.github.com>
2023-06-21 15:03:46 +0530

    Add files via upload

---
+++ Pixelated/extractimag.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import numpy as np
+import os
+
+file_names = ["scrambled1.png", "scrambled2.png"]
+img_data = [np.asarray(Image.open(f'{name}')) for name in file_names]
+
+data = img_data[0].copy() + img_data[1].copy()
+
+new_image = Image.fromarray(data)
+new_image.save("out.png", "PNG")
